394db76ea86daf0558c42562d6969e413e55a9b0
rittersm <rittersm@gmail.com>
2016-08-04 22:40:30 -0400

    controllers with empty methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def require_user
+
+  end
+
+  def current_user
+
+  end
+
 end
---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,11 @@
+class UserSessionsController < ApplicationController
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+end
