fdfefd1e666302b2285a3a72b0fd60d629342721
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-08-05 14:34:30 -0400

    projects REALLY get owners

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,7 @@ class Project < ApplicationRecord
   has_many :timers
   has_many :userprojects
   belongs_to :owner, :class_name => :User, :foreign_key => "owner_id"
+  validates :owner_id, presence: true
 
   validates :title, presence: true, uniqueness: true
 
