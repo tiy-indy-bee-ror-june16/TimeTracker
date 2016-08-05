65b9c752af433ab035e9e9770782d8974d0b8c2a
Marie Denney <mldenne@gmail.com>
2016-08-05 15:18:50 -0400

    Created login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :timers
-  resources :projects
+  resources :projects do
+    resources :timers
+  end
   resources :users
+  post '/login' => 'user_session#create'
+  post '/logout' => 'user_session#destroy'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
