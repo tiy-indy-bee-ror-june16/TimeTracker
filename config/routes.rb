9af4e13187105bddf6a0e341d80808b1a6a55dde
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-08-06 12:40:18 -0400

    bootstrap styles on forms and nested routes created for email invite

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :projects do
     resources :timers
+    resources :users, only: [:show, :create]
   end
   resources :users
   #idk if this is right, but I'm trying to get a page with the timers for a specific user on a specific project.
-  get 'projects/:project_id/users/:id' => 'user#show'
   post '/login' => 'user_sessions#create'
   delete '/logout' => 'user_sessions#destroy'
   post 'projects/assign_user' => 'projects#assign_user'
