ebbef688277e44af147038d9247778cd01246e07
dwegrzen <dwegrzen@gmail.com>
2016-08-07 02:50:25 -0400

    few more small changes, upon creation of timer, page now reloads, timers scoped to created at time

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,11 +5,9 @@ $(document).ready(function() {
   $('.timerstartbutton').on('click', function(ev){
     ev.preventDefault()
     stopwatch("Start")
+    console.log($(this))
+    console.log($('#timer-title').text())
 
-
-
-    // createModal(newUserTemplate,"New User");
-    // $('#newmodal').modal('show')
   })
 
   $('.timerendbutton').on('click', function(ev){
@@ -20,14 +18,11 @@ $(document).ready(function() {
     var timerActualValue = ((+timerstring[0]) * 60 * 60 + (+timerstring[1]) * 60 + (+timerstring[2]))/3600;
     var userid = $(this).parents('.developer-project').find('.userid').text();
     var projectid = $(this).parents('.developer-project').find('.projectid').text();
-    console.log(userid)
-    console.log(projectid)
-    console.log(timerActualValue)
     $.post({
         url: index + "/projects/"+ projectid + "/timers",
         data: {user_id: userid, project_id: projectid, value: timerActualValue},
         success: function(data){
-          console.log(data)
+          location.reload()
         }
     })
 
--- app/models/timer.rb
+++ app/models/timer.rb
@@ -1,5 +1,7 @@
 class Timer < ApplicationRecord
   belongs_to :user
   belongs_to :project
-  
+
+  default_scope { order(created_at: :desc) }
+
 end
