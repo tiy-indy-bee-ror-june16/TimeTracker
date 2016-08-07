9909498cad6143eb831e6a578b59f6199664bd9b
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-08-07 17:17:29 -0400

    trying to fix time format

--- app/controllers/timers_controller.rb
+++ app/controllers/timers_controller.rb
@@ -11,6 +11,25 @@ class TimersController < ApplicationController
 
   def edit
     @timer = Timer.find(params[:id])
+    @timer.value = Time.at(@timer.value).utc.strftime("%H:%M:%S")
   end
+
+  def update
+    @timer = Timer.find(params[:id])
+    x = DateTime.parse(params[:timer][:value])
+    @timer.value = x.hour * 3600 + x.min * 60 + x.sec
+    if @timer.save
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @timer = Timer.find(params[:id])
+    @timer.destroy
+    redirect_to root_path
+  end
+
 
 end
