1a226ada9cbf627675583c8fac09d014a649d850
rittersm <rittersm@gmail.com>
2016-08-07 16:06:04 -0400

    edit timers, needs work

--- app/controllers/timers_controller.rb
+++ app/controllers/timers_controller.rb
@@ -9,4 +9,27 @@ class TimersController < ApplicationController
     redirect_back(fallback_location: root_path)
   end
 
+  def edit
+    @timer = Timer.find(params[:id])
+    @timer.value = Time.at(@timer.value).utc.strftime("%H:%M:%S")
+  end
+
+  def update
+    @timer = Timer.find(params[:id])
+    x = DateTime.parse(@timer.value)
+    @timer.value = x.hour * 3600 + x.min * 60 + x.sec
+    if @timer.save
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @timer = Timer.find(params[:id])
+    @timer.destroy
+    redirect_to root_path
+  end
+
+
 end
