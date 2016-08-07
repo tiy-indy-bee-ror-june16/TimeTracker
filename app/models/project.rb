ba089157d627b682442df4daa9d0919240a2f3fc
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-08-07 12:30:07 -0400

    stashing client stuff

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,8 @@ class Project < ApplicationRecord
   has_many :timers
   has_many :userprojects
   belongs_to :owner, :class_name => :User, :foreign_key => "owner_id"
+  belongs_to :client, :class_name => :User, :foreign_key => "client_id"
+  validates :client_id, presence: true
   validates :owner_id, presence: true
 
   validates :title, presence: true, uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160806172311) do
+ActiveRecord::Schema.define(version: 20160807051633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160806172311) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "owner_id"
+    t.integer  "client_id"
   end
 
   create_table "timers", force: :cascade do |t|
