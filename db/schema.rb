10625a4ff726527bcec1191f21d51293c9541c70
Marie Denney <mldenne@gmail.com>
2016-08-07 20:17:15 -0400

    Ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 20160807131856) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "owner_id"
-    t.integer  "client_id"
   end
 
   create_table "timers", force: :cascade do |t|
